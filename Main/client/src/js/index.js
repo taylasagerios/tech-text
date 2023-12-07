eb5035dfd8d2b6903e2b9c20dea82cd9e2f75333
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-12-06 18:01:07 -0800

    updating the database.js file

--- Main/client/src/js/index.js
+++ Main/client/src/js/index.js
@@ -1,10 +1,32 @@
 import { Workbox } from 'workbox-window';
-import '../css/styles.css';
+import Editor from './editor';
+import './database';
+import '../css/style.css';
+
+const main = document.querySelector('#main');
+main.innerHTML = '';
+
+const loadSpinner = () => {
+  const spinner = document.createElement('div');
+  spinner.classList.add('spinner');
+  spinner.innerHTML = `
+  <div class="loading-container">
+  <div class="loading-spinner" />
+  </div>
+  `;
+  main.appendChild(spinner);
+};
+
+const editor = new Editor();
+
+if (typeof editor === 'undefined') {
+  loadSpinner();
+}
 
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
-  // Register workbox service worker
-  const workboxSW = new Workbox('./src-sw.js');
+  // register workbox service worker
+  const workboxSW = new Workbox('/src-sw.js');
   workboxSW.register();
 } else {
   console.error('Service workers are not supported in this browser.');
